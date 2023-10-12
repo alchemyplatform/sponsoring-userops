26d192f13dd6dfd5709f60a4d58c52758694c15d
Dan Nolan <dan.nolan@alchemy.com>
2023-10-12 14:43:52 -0400

    update aa libs to 0.1.0

--- src/createSigner.ts
+++ src/createSigner.ts
@@ -1,60 +1,53 @@
 // Import the necessary packages
+import { type SmartAccountSigner, LocalAccountSigner } from "@alchemy/aa-core";
 import {
-    type SimpleSmartAccountOwner,
-    SimpleSmartContractAccount,
-    SmartAccountProvider,
-    LocalAccountSigner
-  } from "@alchemy/aa-core";
+  LightSmartContractAccount,
+  getDefaultLightAccountFactory,
+} from "@alchemy/aa-accounts";
+import { AlchemyProvider } from "@alchemy/aa-alchemy";
 import { sepolia } from "viem/chains";
 import * as dotenv from "dotenv";
 dotenv.config();
-import { withAlchemyGasManager } from "@alchemy/aa-alchemy";
 
 // Import the environment variables
 const PRIV_KEY = process.env.PRIV_KEY!;
-const ALCHEMY_API_URL = process.env.ALCHEMY_API_URL!;
+const ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY!;
 const PAYMASTER_POLICY_ID = process.env.PAYMASTER_POLICY_ID!;
 
 // Define the constants
 const ENTRYPOINT_ADDRESS = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
-const SIMPLE_ACCOUNT_FACTORY_ADDRESS =
-  "0x9406Cc6185a346906296840746125a0E44976454";
+const LIGHT_ACCOUNT_FACTORY_ADDRESS = getDefaultLightAccountFactory(sepolia);
 
 /**
  * @description Creates a smart contract account that can be used to send user operations.
  * @returns The smart contract account owner + provider, as a signer, that can be used to send user operations from the SCA
  */
 export async function createSigner() {
-  const owner: SimpleSmartAccountOwner = LocalAccountSigner.privateKeyToAccountSigner(`0x${PRIV_KEY}`);
+  const owner: SmartAccountSigner =
+    LocalAccountSigner.privateKeyToAccountSigner(`0x${PRIV_KEY}`);
 
   const chain = sepolia;
-  const provider = new SmartAccountProvider(
-    ALCHEMY_API_URL,
-    ENTRYPOINT_ADDRESS,
+  const provider = new AlchemyProvider({
+    apiKey: ALCHEMY_API_KEY,
+    entryPointAddress: ENTRYPOINT_ADDRESS,
     chain,
-    undefined,
-    {
-      txMaxRetries: 10,
-      txRetryIntervalMs: 5000,
-    }
-  );
-
-  let signer = provider.connect(
-    (rpcClient) =>
-      new SimpleSmartContractAccount({
-        entryPointAddress: ENTRYPOINT_ADDRESS,
-        chain,
-        owner,
-        factoryAddress: SIMPLE_ACCOUNT_FACTORY_ADDRESS,
-        rpcClient,
-      })
-  );
-
-  // Using Alchemy Gas Manager to sponsor the UserOps
-  signer = withAlchemyGasManager(signer, {
-    policyId: PAYMASTER_POLICY_ID,
-    entryPoint: ENTRYPOINT_ADDRESS,
   });
+
+  let signer = provider
+    .connect(
+      (rpcClient) =>
+        new LightSmartContractAccount({
+          entryPointAddress: ENTRYPOINT_ADDRESS,
+          chain,
+          owner,
+          factoryAddress: LIGHT_ACCOUNT_FACTORY_ADDRESS,
+          rpcClient,
+        })
+    )
+    .withAlchemyGasManager({
+      policyId: PAYMASTER_POLICY_ID,
+      entryPoint: ENTRYPOINT_ADDRESS,
+    });
 
   return signer;
 }
